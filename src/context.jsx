7060b110644e5ba9c1eb026dcb262d45927f9ddb
Sakshi Singh <singhsakshigudiya708@gmail.com>
2024-03-07 22:41:36 +0530

    commit

--- src/context.jsx
+++ src/context.jsx
@@ -1,14 +1,23 @@
 import React, { useContext, useReducer } from "react";
+import reducer from "./reducer";
 const AppContext = React.createContext();
 const initialState = {
     name:"",
     image:"",
 };
-
+const updateHomePage = () => {
+   return dispatch({
+    type:"HOME_UPDATE",
+    payload:{
+   name:"Try to Taste Special Food of the Day",
+   image:"./images/img5.jpg",
+    },
+   }) ;
+};
 
 const AppProvider = ({children}) => {
     const[state,dispatch] = useReducer(reducer,initialState);
-    return<AppContext.Provider value = {{...state}}>{children}</AppContext.Provider>
+    return<AppContext.Provider value = {{...state, updateHomePage}}>{children}</AppContext.Provider>
 };
 
 const useGlobalContext = () => {
