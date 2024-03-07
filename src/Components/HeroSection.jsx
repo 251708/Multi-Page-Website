75f3c81c18a5ad2e8149e0dad5d9dd5828e514fe
Sakshi Singh <singhsakshigudiya708@gmail.com>
2024-03-07 13:05:17 +0530

    commit

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,21 +1,81 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import { Button } from '../styles/Button'
+
 const HeroSection = () => {
   return<Wrapper>
    <div className='container grid grid-two-column'>
     <div className='section-hero-data'>
  <p className='hero-top-data'>THIS IS ME</p>
- <h1 className='hero-heading'>MERI PADHAI</h1>
+ <h1 className='hero-heading'>Enjoy Your Meal</h1>
  <p className='hero-para'>
-    I'm Sakshi Singh A Frontend Developer , youtuber and a Teacher. 
-    You got  so many best teacher on this platform who give u the right direction that makes
-    your life successfull. 
+    I'm Sakshi Singh A chief , youtuber and a Teacher. 
+    You got  so many best meal here u really loved it  that my guarantee you never regret for choosing us
+    your day just become mesmerizing with so many happiness that comes from the food.
  </p>
     </div>
+<Button className = "btn hireme-btn">
+<NavLink to = "/contact"> Explore </NavLink>
 
-    <div className='section-hero-image'></div>
+</Button>
+
+    <div className='section-hero-image'>
+        <picture>
+           <img src="./images/img1.jpg" alt="hero image" className='hero-img' width={500} /> 
+        </picture>
+    </div>
    </div>
   </Wrapper>
 }
-const Wrapper = styled.section``
+const Wrapper = styled.section`
+ padding: 2rem 0;
+
+.section-hero-data {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.btn {
+  max-width: 16rem;
+}
+
+.hero-top-data {
+  text-transform: uppercase;
+  font-weight: 300;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.helper};
+}
+
+.hero-heading {
+  text-transform: uppercase;
+  font-size: 2.4rem;
+}
+
+.hero-para {
+  margin-top: 0.2rem;
+  margin-bottom: 2.0rem;
+  max-width: 60rem;
+}
+
+.section-hero-image {
+  display:flex;
+  justify-content: center;
+  align-items:center;
+}
+
+picture {
+  text-align: center;
+}
+
+.hero-img {
+  max-width: 80%;
+}
+
+@media (max-width: ${({ theme }) => theme.media.mobile}) {
+  .grid {
+    gap: 7.2rem;
+  }
+}`;
 export default HeroSection
